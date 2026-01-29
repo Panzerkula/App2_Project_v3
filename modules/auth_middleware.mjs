fe4e2ad5343335861a65fba3a3a05240c2583f76
Panzerkula <lindland.even@gmail.com>
2026-01-29 11:49:22 +0100

    Changed some stuff in auth middleware

--- modules/auth_middleware.mjs
+++ modules/auth_middleware.mjs
@@ -1,6 +1,6 @@
 export function requireAuth(req, res, next) {
   if (!req.session?.user) {
-    return res.status(401).json({ error: "Login required" });
+    return res.status(401).json("Login required");
   }
   req.user = req.session.user;
   next();
