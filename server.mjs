35641c0f59f3fc1c973c7fdcdf5f686d43ed4bb6
Panzerkula <lindland.even@gmail.com>
2026-02-02 11:56:12 +0100

    Renamed all routes to routers, as the initial spelling was incorrect

--- server.mjs
+++ server.mjs
@@ -1,8 +1,8 @@
 import express from "express";
-import authRoutes from "./routes/auth_routes.mjs";
+import authRouters from "./routers/auth_routers.mjs";
 import { requireAuth } from "./modules/auth_middleware.mjs";
 import session from "express-session";
-import gamesRoutes from "./routes/games_routes.mjs";
+import gamesRouters from "./routers/games_routers.mjs";
 
 const app = express();
 const PORT = 3000;
@@ -17,11 +17,11 @@ app.use(
 
 app.use(express.json());
 
-app.use("/auth", authRoutes);
+app.use("/auth", authRouters);
 
 app.use(express.static("user_app"));
 
-app.use("/games", gamesRoutes);
+app.use("/games", gamesRouters);
 
 app.get("/games", requireAuth, (req, res) => {
   res.json({ message: "You are logged in", user: req.user });
