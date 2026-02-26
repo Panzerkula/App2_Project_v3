4777e9b6d597718c3d6bfeede7de7af0babefa62
Panzerkula <lindland.even@gmail.com>
2026-02-26 17:49:19 +0100

    edited sql-table for create users

--- databases/users/createTable.sql
+++ databases/users/createTable.sql
@@ -1,8 +1,8 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     profile_pic TEXT DEFAULT '/assets/no_pic.png',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
